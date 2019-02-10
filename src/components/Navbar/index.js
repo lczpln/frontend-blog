c6253d55ed5e1ac913e7849907381e9ce2c8ca60
Lczpln <lucaspellison@gmail.com>
2019-02-10 20:30:39 -0200

    Mobile css

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,26 +3,49 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
 export default class Navbar extends Component {
+	state = {
+		collapsed: false,
+	}
+
+	collapseNavbar = () => {
+		this.setState({ collapsed: !this.state.collapsed });
+	}
+
+	selectUserComponent = (classname) => {
+		return (
+			<ul className={classname}>
+				<li>
+					<NavLink activeClassName="navlink-active-class" onClick={this.collapseNavbar} exact to="/">All Posts</NavLink>
+				</li>
+				<li>
+					<NavLink to="/post" onClick={(e) => e.preventDefault()} activeClassName="navlink-active-class">Post</NavLink>
+				</li>
+				<li>
+					<NavLink activeClassName="navlink-active-class" onClick={this.collapseNavbar} to="/create">Create</NavLink>
+				</li>
+			</ul>
+		);
+	}
+
 	render() {
 		return (
 			<header>
 				<div className="center-navbar-div" id="header">
 					<div className="left-navbar-div" onClick={() => this.props.history.push('/')}>
-						<img src={require('../../images/blog.svg')} width={30} alt="blog_logo"/>
+						<img src={require('../../images/blog.svg')} width={30} alt="blog_logo" />
 						<h1>Blog</h1>
 					</div>
 					<div className="right-navbar-div">
-						<ul>
-							<li>
-								<NavLink activeClassName="navlink-active-class" exact to="/">All Posts</NavLink>
-							</li>
-							<li>
-								<NavLink to="/post" onClick={(e) => e.preventDefault()} activeClassName="navlink-active-class">Post</NavLink>
-							</li>
-							<li>
-								<NavLink activeClassName="navlink-active-class" to="/create">Create</NavLink>
-							</li>
-						</ul>
+						{this.state.collapsed
+							? <i class="fas fa-times" id="close" onClick={this.collapseNavbar}></i>
+							: <i class="fas fa-bars" id="hamburgger" onClick={this.collapseNavbar}></i>
+
+						}
+						{this.selectUserComponent("desktop")}
+						{this.state.collapsed
+							? this.selectUserComponent("mobile")
+							: null
+						}
 					</div>
 				</div>
 			</header>
